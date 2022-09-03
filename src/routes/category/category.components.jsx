4ec06e6685c4da46e846f3659b5cf1443fb65042
Cristian <cristian@fysight.ai>
2022-09-03 11:37:15 -0300

    :hammer: turn category scss into styled component

--- src/routes/category/category.components.jsx
+++ src/routes/category/category.components.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
 import { CategoriesContext } from "../../contexts/categories.context";
 
-import "./category.styles.scss";
+import "./category.styles.jsx";
+import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
 
 const Category = () => {
     const { category } = useParams();
@@ -19,10 +20,10 @@ const Category = () => {
 
     return (
         <Fragment>
-            <h2 className="category-title">{category.toLocaleUpperCase()}</h2>
-            <div className="category-container">
+            <CategoryTitle>{category.toLocaleUpperCase()}</CategoryTitle>
+            <CategoryContainer>
                 {products && products.map((product) => <ProductCard key={product.id} product={product} />)}
-            </div>
+            </CategoryContainer>
         </Fragment>
     );
 };
