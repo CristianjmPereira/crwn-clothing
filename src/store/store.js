6400421047567ddeb7679f1efd48a704949cae94
Cristian <cristian@fysight.ai>
2022-09-04 19:06:45 -0300

    :sparkles: add redux state persistense

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,37 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
+
+// Middleware example
+// const loggerMiddleware = (state) => (next) => (action) => {
+//     if (!action.type) {
+//         return next(action);
+//     }
+
+//     console.log('type: ', action.type);
+//     console.log('payload: ', action.payload);
+//     console.log('currentState: ', state);
+
+//     next(action);
+
+//     console.log('next state: ', state.getState());
+// }
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [logger];
 
 const composeEnhancers = compose(applyMiddleware(...middleWares))
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store);
