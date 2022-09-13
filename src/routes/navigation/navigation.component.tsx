bc713967002f8aa4d8d95cfa2e030906150a24a9
Cristian <cristian@fysight.ai>
2022-09-13 10:59:35 -0300

    :hammer: add types to svg components

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -24,7 +24,7 @@ const Navigation = () => {
         <Fragment>
             <NavigationContainer>
                 <LogoContainer to="/">
-                    <CrwnLogo alt="Crwn Logo" />
+                    <CrwnLogo className="logo" />
                 </LogoContainer>
                 <NavLinks>
                     <NavLink to="/shop">
